eb33caf5378f94c4a21a6512f3e567a2481a7ab0
wooyonghwa <qhdl0224@gmail.com>
2020-12-16 18:28:01 +0900

    42840

---
+++ 42840.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(void) {
+    vector<int> answer = { 1, 2, 3, 4, 5 };
+    solution(answer);
+    return 0;
+}
+
+vector<int> solution(vector<int> answers) {
+    vector<int> answer;
+    return answer;
+}
